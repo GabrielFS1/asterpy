77c63d17aaf04a5766b8bdc7a203f9c6ffe63672
Gabriel S <fernandesg.s@hotmail.com>
2022-04-05 06:23:40 -0300

    MADE so auto process accept images

--- auto_histo.py
+++ auto_histo.py
@@ -1,3 +1,5 @@
+import glob
+import shutil
 import subprocess
 import cv2
 import rasterio
@@ -6,7 +8,7 @@ import sqlite3
 import os
 from osgeo import gdal
 import numpy as np
-
+from PIL import Image
 
 
 # Gets the mean values (max and min of the database)
@@ -14,8 +16,20 @@ db = sqlite3.connect("aster_dados.db", uri=True)
 rows = db.execute("SELECT * FROM aster_data").fetchall()
 
 
+"""
+def normal_min_max_mean(type: str='phyll'):
+    if type not in ['phyll', 'npv', 'qtz']:
+        raise ValueError("O tipo específicado não é valido (phyll, npv ou qtz)")
 
-def normal_min_max_mean(index: int=5):
+    sum = qtd = 0
+
+    if type == 'phyll':
+        index = 5
+    elif type == 'npv':
+        index = 6
+    elif type == 'qtz':
+        index = 7
+
     sum = qtd = 0
 
     # Calculates the minimum value from the database
@@ -35,54 +49,34 @@ def normal_min_max_mean(index: int=5):
     max = sum / qtd
 
     return min, max
+"""
 
+def get_min_max(type: str='phyll', period: str='dia'):
 
-def day_min_max_mean(index: int=5):
-    sum = qtd = 0
+    if type not in ['phyll', 'npv', 'qtz']:
+        raise ValueError(f"O tipo específicado não é valido. O valor deve ser ['phyll', 'npv', 'qtz']")
 
-    # Calculates the minimum value from the database
-    for row in rows:
-        if row[index]:
-            if row[4] == 'dia':
-                sum += float(row[index].split(':')[0])
-                qtd += 1
-    min = sum / qtd
+    if period not in ['dia', 'noite']:
+        raise ValueError(f"O valor específicado {period} não é valido. O valor deve ser ['dia', 'noite']")
 
-    sum = qtd = 0
+    sum_min = sum_max = qtd = 0
 
-    # Calculates the maximum value from the database
-    for row in rows:
-        if row[index]:
-            if row[4] == 'dia':
-                sum += float(row[index].split(':')[1])
-                qtd += 1
-    max = sum / qtd
-
-    return min, max
-
-def night_min_max_mean(index: int=5):
-    sum = qtd = 0
+    # Sets index with the index number of the column in the database
+    if type == 'phyll':
+        index = 5
+    elif type == 'npv':
+        index = 6
+    elif type == 'qtz':
+        index = 7
 
     # Calculates the minimum value from the database
     for row in rows:
         if row[index]:
-            if row[4] == 'noite':
-                sum += float(row[index].split(':')[0])
+            if row[4] == period:
+                sum_min += float(row[index].split(':')[0]) 
+                sum_max += float(row[index].split(':')[1]) 
                 qtd += 1
-    min = sum / qtd
-
-    sum = qtd = 0
-
-    # Calculates the maximum value from the database
-    for row in rows:
-        if row[index]:
-            if row[4] == 'noite':
-                sum += float(row[index].split(':')[1])
-                qtd += 1
-    max = sum / qtd
-
-    return min, max
-
+    return sum_min / qtd, sum_max / qtd
 
 
 """
@@ -92,16 +86,43 @@ index
 6 - npv
 7 - qtz
 """
-print(normal_min_max_mean(5))
-print(day_min_max_mean(5))
-print(night_min_max_mean(5))
+print(f"Phyll day: {get_min_max()}")
+print(f"Npv day: {get_min_max('npv', 'dia')}")
+print(f"Qtz day: {get_min_max('qtz', 'dia')}")
+print(f"Phyll night: {get_min_max('phyll', 'noite')}")
+print(f"Npv night: {get_min_max('npv', 'noite')}")
+print(f"Qtz night: {get_min_max('qtz', 'noite')}")
 
 
 files = os.listdir("00_Arquivos")
 
-for file in files:
+control = 0
 
-    if file.split('.')[-1] == 'zip':
+for i in range(len(files)):
+    file = files[i]
+    print(file)
+    print(f"control: {control}")
+    # Checks if the image period is day or night
+    #Gets the image period (day/night) to define the histogram value
+    infos = file.split('_')[2]
+    hour = int(infos[11:13])
+    if hour >= 6 and hour < 18:
+        period = 'dia'
+    else:
+        period = 'noite'
+
+    if control == 0:
+        type = 'Phyll'
+        min, max = get_min_max('phyll', period)
+    elif control == 1:
+        type = 'Npv'
+        min, max = get_min_max('npv', period)
+    elif control == 2:
+        type = 'Qtz'
+        min, max = get_min_max('qtz', period)
+
+
+    if file.split('.')[-1] == 'zip' or file.split('.')[-1]  == 'met':
         continue
 
     layer_dir = 'Teste\\'
@@ -159,77 +180,59 @@ for file in files:
     # Ignora operações inválidas
     np.seterr(divide='ignore', invalid='ignore')
 
-    # Cálculo do índice termal de filossilicatos (Vicente & Souza Filho, 2010)
-    phyllosilicates = AST10/AST11*AST12
-    phyllosilicates = np.ma.masked_invalid(phyllosilicates)
-
-    # Cálculo do índice termal de vegetação não fotossinteticamente ativa (Vicente et al., 2017)
-    npvthm = AST11/(AST13 + AST14)
-    npvthm = np.ma.masked_invalid(npvthm)
-
-    # Cálculo do índice termal de quartzo (Rockwell & Hofstra, 2008)
-    quartz = AST11/(AST10 + AST12)*AST13/AST12
-    quartz = np.ma.masked_invalid(quartz)
-
-    # Altera os metadados da imagem para float e determina 1 banda para a imagem
-    ras_meta['dtype'] = rasterio.float32
-    ras_meta['count'] = 1
-
     descriptions = [
     'Band Math AST10/AST11*AST12', # Descrição filossilicatos
     'Band Math AST11/(AST13 + AST14)', # Descrição npvthm
     'Band Math AST11/(AST10 + AST12)*AST13/AST12'] # Descrição quartzo
 
-    with rasterio.open("Teste\\" + file + '_Phyll.tif', 'w', **ras_meta) as dst1:
-        # Define o array da banda como o array phyll
-        dst1.write(phyllosilicates.astype(rasterio.float32), 1)
-        dst1.set_band_description(1, descriptions[0]) # Escreve a descrição de cada banda
+    # Altera os metadados da imagem para float e determina 1 banda para a imagem
+    ras_meta['dtype'] = rasterio.float32
+    ras_meta['count'] = 1
 
-    with rasterio.open("Teste\\" + file + '_Npv.tif', 'w', **ras_meta) as dst1:
-        # Define o array da banda como o array npv
-        dst1.write(npvthm.astype(rasterio.float32), 1)
-        # Escreve a descrição da banda
-        dst1.set_band_description(1, descriptions[1])
+    if control == 0:
+        # Cálculo do índice termal de filossilicatos (Vicente & Souza Filho, 2010)
+        phyllosilicates = AST10/AST11*AST12
+        phyllosilicates = np.ma.masked_invalid(phyllosilicates)
+        with rasterio.open("Teste\\" + file + '_Phyll.tif', 'w', **ras_meta) as dst1:
+            # Define o array da banda como o array phyll
+            dst1.write(phyllosilicates.astype(rasterio.float32), 1)
+            dst1.set_band_description(1, descriptions[0]) # Escreve a descrição de cada banda
 
-    with rasterio.open("Teste\\" + file + '_Qtz.tif', 'w', **ras_meta) as dst1:
-        # Define o array da banda como o array qtz
-        dst1.write(quartz.astype(rasterio.float32), 1)
-        # Escreve a descrição da banda
-        dst1.set_band_description(1, descriptions[2])
+    elif control == 1:
+        # Cálculo do índice termal de vegetação não fotossinteticamente ativa (Vicente et al., 2017)
+        npvthm = AST11/(AST13 + AST14)
+        npvthm = np.ma.masked_invalid(npvthm)
+        with rasterio.open("Teste\\" + file + '_Npv.tif', 'w', **ras_meta) as dst1:
+            # Define o array da banda como o array npv
+            dst1.write(npvthm.astype(rasterio.float32), 1)
+            # Escreve a descrição da banda
+            dst1.set_band_description(1, descriptions[1])
+
+    elif control == 2:
+        # Cálculo do índice termal de quartzo (Rockwell & Hofstra, 2008)
+        quartz = AST11/(AST10 + AST12)*AST13/AST12
+        quartz = np.ma.masked_invalid(quartz)
+        with rasterio.open("Teste\\" + file + '_Qtz.tif', 'w', **ras_meta) as dst1:
+            # Define o array da banda como o array qtz
+            dst1.write(quartz.astype(rasterio.float32), 1)
+            # Escreve a descrição da banda
+            dst1.set_band_description(1, descriptions[2])
+
     ds.close()
 
-
     # Shows the original image
-    img_in = "Teste\\" + file + "_Phyll.tif"
+    img_in = f"Teste\\{file}_{type}.tif"
     min, max = ap.histogram.get_histogram_range(img_in)
-    subprocess.call(['gdal_translate.exe', '-ot', 'Byte', '-quiet', '-scale', str(min), str(max), img_in, "Teste\\" + file + "original_Phyll.tif"])
-
-
-    #Gets the image period (day/night) to define the histogram value
-    infos = file.split('_')[2]
-    hour = int(infos[11:13])
-    if hour >= 6 and hour < 18:
-        period = 'dia'
-        min, max = day_min_max_mean()
-    else:
-        period = 'noite'
-        min, max = night_min_max_mean()
+    subprocess.call(['gdal_translate.exe', '-ot', 'Byte', '-quiet', '-scale', str(min), str(max), img_in, f"Teste\\{file}original_{type}.tif"])
 
     # Makes the threshold clipping
-    subprocess.call(['gdal_translate.exe', '-ot', 'Byte', '-quiet' ,'-scale', str(min), str(max), img_in, "Teste\\" + file + "processed_Phyll.tif"])
+    subprocess.call(['gdal_translate.exe', '-ot', 'Byte', '-quiet' ,'-scale', str(min), str(max), img_in, f"Teste\\{file}processed_{type}.tif"])
 
-    #img_in = "Teste\\" + file + "_Npv"
-    #min, max = ap.histogram.get_histogram_range("Teste\\" + file + '_Npv.tif')
-    #subprocess.call(['gdal_translate.exe', '-ot', 'Byte', '-quiet', '-scale', str(min), str(max), img_in, index_dir_fig + dir + file + index])
+    img = cv2.imread(f"Teste\\{file}original_{type}.tif")
+    img2 = cv2.imread(f"Teste\\{file}processed_{type}.tif")
 
-
-    #img_in = "Teste\\" + file + "_Qtz"
-    #min, max = ap.histogram.get_histogram_range("Teste\\" + file + '_Qtz.tif')
-    #subprocess.call(['gdal_translate.exe', '-ot', 'Byte', '-quiet', '-scale', str(min), str(max), img_in, index_dir_fig + dir + file + index])
-
-
-    img = cv2.imread("Teste\\" + file + "original_Phyll.tif")
-    img2 = cv2.imread("Teste\\" + file + "processed_Phyll.tif")
+    # Apllies the median filter
+    #img2 = cv2.medianBlur(img2, 3)
 
     cv2.namedWindow(file + "_Original", cv2.WINDOW_NORMAL)
     cv2.namedWindow(file + "_Processed", cv2.WINDOW_NORMAL)
@@ -237,6 +240,28 @@ for file in files:
     cv2.imshow(file + "_Original", img)
     cv2.imshow(file + "_Processed", img2)
 
+    # Tries to open the image manually processed
+    try:
+        im = Image.open(f"03_Histograma\\0{control + 1}_{type}\\{file}_Histo_{type}.tif")
+        im.show()
+
+    except FileExistsError:
+        pass
+
     cv2.waitKey()
 
     cv2.destroyAllWindows()
+
+    folder = 'Teste\\'
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            #print('Failed to delete %s. Reason: %s' % (file_path, e))
+            pass
+
+    control += 1
