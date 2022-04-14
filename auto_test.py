a90ccd20e2ab6b2095594ce8e995acef829b7680
Gabriel S <fernandesg.s@hotmail.com>
2022-04-13 22:51:14 -0300

    MADE text file for auto process

--- auto_test.py
+++ auto_test.py
@@ -11,29 +11,38 @@ import numpy as np
 from PIL import Image
 
 
-inpath = "02_Indices\\01_Phyll\\"
+inpath = "02_Indices\\03_Qtz\\"
 
 imgs = os.listdir(inpath)
 
-img = imgs[4]
-# Open the image
-im = Image.open(inpath + img)
+file = open('values.txt', 'a')
 
-# Reads the image as an array
-arr = np.array(im.getdata())
+for i in range(10):
+    img = imgs[i]
+    # Open the image
+    im = Image.open(inpath + img)
 
-# Removes the zeros of the array and sorts the array
-arr_nonzero = np.sort(np.ma.masked_equal(arr, 0))
+    # Reads the image as an array
+    arr = np.array(im.getdata())
 
-# Gets the minimun value for threshold
-min = np.nanquantile(arr_nonzero, q=0.75)
+    # Removes the zeros of the array and sorts the array
+    arr_nonzero = np.sort(np.ma.masked_equal(arr, 0))
 
-# Gets the maximum value for threshold
-max = np.nanmax(np.ma.masked_invalid(arr_nonzero))
+    # Gets the minimun value for threshold
+    min = np.nanquantile(arr_nonzero, q=0.75)
 
-subprocess.call(['gdal_translate.exe', '-ot', 'Byte', '-quiet', '-scale',
-                str(min), str(max), inpath + img, f"Teste\\Histo_{img}"])
+    # Gets the maximum value for threshold
+    max = np.nanmax(np.ma.masked_invalid(arr_nonzero))
 
-img = cv2.imread(f"Teste\\Histo_{img}", 0)
+    subprocess.call(['gdal_translate.exe', '-ot', 'Byte', '-quiet', '-scale',
+                    str(min), str(max), inpath + img, f"Teste\\Histo_{img}"])
 
-img_median = cv2.medianBlur(img, 3)
+    img = cv2.imread(f"Teste\\Histo_{img}", 0)
+
+    img_median = cv2.medianBlur(img, 3)
+
+    file.write(f"{imgs[i]} - {min} : {max}\n")
+
+    #cv2.imshow('image', img)
+
+    #cv2.waitKey()
