3658522be8872be03f5d329843ef3b6b0777b860
Gabriel S <fernandesg.s@hotmail.com>
2022-04-17 10:24:37 -0300

    UPDATED quantile program

--- auto_quantile.py
+++ auto_quantile.py
@@ -11,38 +11,54 @@ import numpy as np
 from PIL import Image
 
 
-inpath = "02_Indices\\03_Qtz\\"
 
-imgs = os.listdir(inpath)
+for i in range(3):
 
-file = open('values.txt', 'a')
+    percentage = 10
 
-for i in range(10):
-    img = imgs[i]
-    # Open the image
-    im = Image.open(inpath + img)
+    if i == 0:
+        inpath = "02_Indices\\01_Phyll\\"
+        file = open(f'Teste\\{percentage}%_Phyll.txt', 'w')
 
-    # Reads the image as an array
-    arr = np.array(im.getdata())
+    if i == 1:
+        inpath = "02_Indices\\02_Npv\\"
+        file = open(f'Teste\\{percentage}%_Npv.txt', 'w')
 
-    # Removes the zeros of the array and sorts the array
-    arr_nonzero = np.sort(np.ma.masked_equal(arr, 0))
+    if i == 2:
+        inpath = "02_Indices\\03_Qtz\\"
+        file = open(f'Teste\\{percentage}%_Qtz.txt', 'w')
 
-    # Gets the minimun value for threshold
-    min = np.nanquantile(arr_nonzero, q=0.95)
 
-    # Gets the maximum value for threshold
-    max = np.nanmax(np.ma.masked_invalid(arr_nonzero))
+    imgs = os.listdir(inpath)
 
-    subprocess.call(['gdal_translate.exe', '-ot', 'Byte', '-quiet', '-scale',
-                    str(min), str(max), inpath + img, f"Teste\\Histo_5%_{img}"])
+    for i in range(15):
+        img = imgs[i]
+        # Open the image
+        im = Image.open(inpath + img)
 
-    img = cv2.imread(f"Teste\\Histo_{img}", 0)
+        # Reads the image as an array
+        arr = np.array(im.getdata())
 
-    img_median = cv2.medianBlur(img, 3)
+        # Removes the zeros of the array and sorts the array
+        arr_nonzero = np.sort(np.ma.masked_equal(arr, 0))
 
-    file.write(f"5% - {imgs[i]} - {min} : {max}\n")
 
-    #cv2.imshow('image', img)
+        print(f"calculating for {1 - percentage/100}")
+        # Gets the minimun value for threshold
+        min = np.nanquantile(arr_nonzero, q=(1 - percentage/100))
 
-    #cv2.waitKey()
+        # Gets the maximum value for threshold
+        max = np.nanmax(np.ma.masked_invalid(arr_nonzero))
+
+        subprocess.call(['gdal_translate.exe', '-ot', 'Byte', '-quiet', '-scale',
+                        str(min), str(max), inpath + img, f"Teste\\{percentage}%_Histo_{img}"])
+
+        #img = cv2.imread(f"Teste\\05%_Histo_{img}", 0)
+
+        #img_median = cv2.medianBlur(img, 3)
+
+        file.write(f"{percentage}% - {imgs[i]} - {min} : {max}\n")
+
+        #cv2.imshow('image', img)
+
+        #cv2.waitKey()
