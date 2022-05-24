b1ab38a53d79758d6aa6070a4595f194e8e33330
Gabriel S <fernandesg.s@hotmail.com>
2022-05-24 17:45:43 -0300

    FIXED key name

--- asterpy/directories.py
+++ asterpy/directories.py
@@ -5,16 +5,16 @@ def directory_maker():
     """Creates the """
 
     dirs = [
-        {'name:': '00_Arquivos', 'sub_folders': False},
-        {'name:': '01_Layer_Stacking', 'sub_folders': False},
-        {'name:': '02_Indices', 'sub_folders': True},
-        {'name:': '02_Indices\\Figuras\\', 'sub_folders': True},
-        {'name:': '03_Histograma', 'sub_folders': True},
-        {'name:': '04_Mediana', 'sub_folders': True},
-        {'name:': '05_RGB', 'sub_folders': True},
-        {'name:': '06_Triplete', 'sub_folders': True},
-        {'name:': '07_Fotos_histograma', 'sub_folders': True},
-        {'name:': '08_Shapes', 'sub_folders': False}
+        {'name': '00_Arquivos', 'sub_folders': False},
+        {'name': '01_Layer_Stacking', 'sub_folders': False},
+        {'name': '02_Indices', 'sub_folders': True},
+        {'name': '02_Indices\\Figuras\\', 'sub_folders': True},
+        {'name': '03_Histograma', 'sub_folders': True},
+        {'name': '04_Mediana', 'sub_folders': True},
+        {'name': '05_RGB', 'sub_folders': True},
+        {'name': '06_Triplete', 'sub_folders': True},
+        {'name': '07_Fotos_histograma', 'sub_folders': True},
+        {'name': '08_Shapes', 'sub_folders': False}
     ]
 
     sub_dirs = [
