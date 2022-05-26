0bf4fa794cc165fab5bf9b6722539a30ae4c1075
Gabriel S <fernandesg.s@hotmail.com>
2022-05-26 20:21:52 -0300

    GET updated auto file

--- auto_histo.py
+++ auto_histo.py
@@ -1,6 +1,7 @@
 import glob
 import shutil
 import subprocess
+from typing import List
 import cv2
 import rasterio
 import asterpy as ap
@@ -9,249 +10,162 @@ import os
 from osgeo import gdal
 import numpy as np
 from PIL import Image
+import statistics
 
 
-# Gets the mean values (max and min of the database)
-db = sqlite3.connect("aster_dados.db", uri=True)
-rows = db.execute("SELECT * FROM aster_data").fetchall()
+def get_min_max(type: str=None, period: str=None, months: str=None):
+    db = sqlite3.connect("D:\\GG\\git\\asterpy\\aster_dados.db", uri=True)
+    rows = db.execute("SELECT * FROM aster_data").fetchall()
 
+    if type:
+        if type not in ['phyll', 'npv', 'qtz']:
+            raise ValueError(f"======== The type {type} it is not valid. The value must be 'phyll', 'npv' or 'qtz' ========")
 
-"""
-def normal_min_max_mean(type: str='phyll'):
-    if type not in ['phyll', 'npv', 'qtz']:
-        raise ValueError("O tipo específicado não é valido (phyll, npv ou qtz)")
+        # Sets index with the index number of the column in the database
+        if type == 'phyll':
+            index = 5
+        elif type == 'npv':
+            index = 6
+        elif type == 'qtz':
+            index = 7
+    else:
+        return print("======== Please inform the index type ========")
 
-    sum = qtd = 0
+    if period:
+        if period not in ['dia', 'noite']:
+            raise ValueError(f"======== The value {period} it is not valid. The value must be 'dia' or 'noite' ========")
 
-    if type == 'phyll':
-        index = 5
-    elif type == 'npv':
-        index = 6
-    elif type == 'qtz':
-        index = 7
+    if months:
+        if months not in ['chuvoso', 'seco']:
+            raise ValueError(f"======== The value {months} it is not valid. The value must be 'chuvoso' or 'seco' ========")
 
-    sum = qtd = 0
+        if months == 'chuvoso':
+            months = [3, 4, 5, 6, 7, 8, 9] # Mar - Set
+        elif months == 'seco':
+            months = [10, 11, 12, 1, 2] # Oct - Feb
 
+    min_values: List[float] = []
+    max_values: List[float] = []
     # Calculates the minimum value from the database
     for row in rows:
         if row[index]:
-            sum += float(row[index].split(':')[0])
-            qtd += 1
-    min = sum / qtd
+            img_period = row[4]
+            img_month = int(row[1].split('_')[2][3: 4 + 1])
 
-    sum = qtd = 0
-
-    # Calculates the maximum value from the database
-    for row in rows:
-        if row[index]:
-            sum += float(row[index].split(':')[1])
-            qtd += 1
-    max = sum / qtd
-
-    return min, max
-"""
-
-def get_min_max(type: str='phyll', period: str='dia'):
-
-    if type not in ['phyll', 'npv', 'qtz']:
-        raise ValueError(f"O tipo específicado não é valido. O valor deve ser ['phyll', 'npv', 'qtz']")
-
-    if period not in ['dia', 'noite']:
-        raise ValueError(f"O valor específicado {period} não é valido. O valor deve ser ['dia', 'noite']")
-
-    sum_min = sum_max = qtd = 0
-
-    # Sets index with the index number of the column in the database
-    if type == 'phyll':
-        index = 5
-    elif type == 'npv':
-        index = 6
-    elif type == 'qtz':
-        index = 7
-
-    # Calculates the minimum value from the database
-    for row in rows:
-        if row[index]:
-            if row[4] == period:
-                sum_min += float(row[index].split(':')[0]) 
-                sum_max += float(row[index].split(':')[1]) 
-                qtd += 1
-    return sum_min / qtd, sum_max / qtd
-
-
-"""
-index
-
-5 - phyll
-6 - npv
-7 - qtz
-"""
-print(f"Phyll day: {get_min_max()}")
-print(f"Npv day: {get_min_max('npv', 'dia')}")
-print(f"Qtz day: {get_min_max('qtz', 'dia')}")
-print(f"Phyll night: {get_min_max('phyll', 'noite')}")
-print(f"Npv night: {get_min_max('npv', 'noite')}")
-print(f"Qtz night: {get_min_max('qtz', 'noite')}")
-
-
-files = os.listdir("00_Arquivos")
-
-control = 0
-
-for file in files:
-    while (control < 3):
-        print(f"control: {control}")
-        # Checks if the image period is day or night
-        #Gets the image period (day/night) to define the histogram value
-        infos = file.split('_')[2]
-        hour = int(infos[11:13])
-        if hour >= 6 and hour < 18:
-            period = 'dia'
-        else:
-            period = 'noite'
-
-        if control == 0:
-            type = 'Phyll'
-            min, max = get_min_max('phyll', period)
-        elif control == 1:
-            type = 'Npv'
-            min, max = get_min_max('npv', period)
-        elif control == 2:
-            type = 'Qtz'
-            min, max = get_min_max('qtz', period)
-
-
-        if file.split('.')[-1] == 'zip' or file.split('.')[-1]  == 'met':
-            print(f"{file} arquivo inválido")
-            break
-
-        layer_dir = 'Teste\\'
-        files_dir = '00_Arquivos\\'
-
-        # Cria lista com os caminhos de cada banda
-        band_list = []
-        band_files = os.listdir(files_dir + file)
-        for i in band_files:
-            # Verifica se o arquivo é .tif
-            if i.split('.')[-1] != 'tif':
+            if period and img_period != period:
                 continue
+
+            if months and img_month not in months:
+                continue
+
+            min_values.append(float(row[index].split(':')[0]))
+            max_values.append(float(row[index].split(':')[1]))
+
+    mean_min = statistics.mean(min_values)
+    mean_max = statistics.mean(max_values)
+
+    median_min = statistics.median(min_values)
+    median_max = statistics.median(max_values)
+
+    min = median_min
+    max = median_max
+    return min, max
+
+def get_period(filename: str=None):
+    if not filename:
+        raise ValueError(f"Please, inform a valid image")
+
+    hour = int(filename.split('_')[2][11:13])
+    if hour >= 6 and hour < 18:
+        return 'dia'
+    else:
+        return 'noite'
+
+def get_months(filename: str=None):
+    """
+    
+    returns 'chuvoso' (3, 4, 5, 6, 7, 8, 9) ou 'seco' (1, 2, 10, 11, 12)"""
+    if not filename:
+        raise ValueError(f"Please, inform a valid image")
+
+    month = int(filename.split('_')[2][3:4 + 1])
+    if month in [3, 4, 5, 6, 7, 8, 9]: # mar - sep
+        return 'chuvoso'
+    else:
+        return 'seco'
+
+
+def auto_process():
+    print(f"Phyll dia chuvoso: {get_min_max('phyll', 'dia', 'chuvoso')}")
+    print(f"Phyll dia seco: {get_min_max('phyll', 'dia', 'seco')}")
+    print(f"Phyll noite chuvoso: {get_min_max('phyll', 'noite', 'chuvoso')}")
+    print(f"Phyll noite seco: {get_min_max('phyll', 'noite', 'seco')}")
+    print('='*50)
+    print(f"Npv dia chuvoso: {get_min_max('npv', 'dia', 'chuvoso')}")
+    print(f"Npv dia seco: {get_min_max('npv', 'dia', 'seco')}")
+    print(f"Npv noite chuvoso: {get_min_max('npv', 'noite', 'chuvoso')}")
+    print(f"Npv noite seco: {get_min_max('npv', 'noite', 'seco')}")
+    print('='*50)
+    print(f"Qtz dia chuvoso: {get_min_max('qtz', 'dia', 'chuvoso')}")
+    print(f"Qtz dia seco: {get_min_max('qtz', 'dia', 'seco')}")
+    print(f"Qtz noite chuvoso: {get_min_max('qtz', 'noite', 'chuvoso')}")
+    print(f"Qtz noite seco: {get_min_max('qtz', 'noite', 'seco')}")
+
+    for i in range(3):
+
+        if i == 0:
+            inpath = "02_Indices\\01_Phyll\\"
+            type = "phyll"
+
+            #file = open(f'Teste\\Histo_mean_Phyll.txt', 'w')
+
+        if i == 1:
+            inpath = "02_Indices\\02_Npv\\"
+            type = "npv"
+
+            #file = open(f'Teste\\Histo_mean_Npv.txt', 'w')
+
+        if i == 2:
+            inpath = "02_Indices\\03_Qtz\\"
+            type = "qtz"
+            #file = open(f'Teste\\_Qtz.txt', 'w')
+
+        files = os.listdir(inpath)
+
+        for i in range(len(files)):
+            file = files[i]
+
+            hour = int(file.split('_')[2][11:13])
+            month = int(file.split('_')[2][3:4 + 1])
+
+            if month in [3, 4, 5, 6, 7, 8, 9]: # mar - sep
+                months = 'chuvoso'
             else:
-                # Garante que é o arquivo com a banda correta (10-14)
-                if len(i.split('.')[2]) == 11 and i.split('.')[2][9] == '1' and i.split('.')[2][10] in '01234':
-                    band_list.append(files_dir + file + '\\' + i)
+                months = 'seco'
 
-        # Lê os metadados do primeiro arquivo
-        with rasterio.open(band_list[0]) as src0:
-            meta = src0.meta
+            if hour >= 6 and hour < 18:
+                period = 'dia'
+            else:
+                period = 'noite'
 
-        # Atualiza numero de camadas e o tipo dos dados
-        meta.update(count = len(band_list), dtype='uint16')
+            # Makes the threshold clipping
+            min, max = get_min_max(type, period, months)
+            subprocess.call(['gdal_translate.exe', '-ot', 'Byte', '-quiet' ,'-scale', str(min), str(max), f"{inpath}\\{file}", f"Teste\\Histo_{file}"])
 
-        descriptions = ['Band 10', 'Band 11', 'Band 12', 'Band 13', 'Band 14']
+            #im = Image.open(f"03_Histograma\\0{i + 1}_{type}\\{file}_Histo_{}.tif")
+            #im.show()
 
-        # Lê cada camada do arquivo e escreve em um arquivo único
-        with rasterio.open(layer_dir + file + '_Layer_Stacking_sem_rotacao.tif', 'w', **meta) as dst:
-            for id, layer in enumerate(band_list, start=1):
-                with rasterio.open(layer) as src1:
-                    dst.write_band(id, src1.read(1))
-                    # Adiciona descrição da banda de acordo com a lista descriptions
-                    dst.set_band_description(id, descriptions[id-1])
-
-        # Rotaciona a imagem a partir dos metadados
-        rotated_image = gdal.Warp(layer_dir + file + '_Layer_Stacking.tif', layer_dir + file + '_Layer_Stacking_sem_rotacao.tif')
-        os.remove(layer_dir + file + '_Layer_Stacking_sem_rotacao.tif')
-
-
-        # === Generates the index images === #
-        layer_dir = 'Teste\\'
-
-        ds = rasterio.open(layer_dir + file + '_Layer_Stacking.tif')
-
-        # Le cada banda do arquivo raster layer stack
-        AST10 = ds.read(1).astype(float)
-        AST11 = ds.read(2).astype(float)
-        AST12 = ds.read(3).astype(float)
-        AST13 = ds.read(4).astype(float)
-        AST14 = ds.read(5).astype(float)
-
-        # Lê os metadados do arquivo original
-        ras_meta = ds.profile
-
-        # Ignora operações inválidas
-        np.seterr(divide='ignore', invalid='ignore')
-
-        descriptions = [
-        'Band Math AST10/AST11*AST12', # Descrição filossilicatos
-        'Band Math AST11/(AST13 + AST14)', # Descrição npvthm
-        'Band Math AST11/(AST10 + AST12)*AST13/AST12'] # Descrição quartzo
-
-        # Altera os metadados da imagem para float e determina 1 banda para a imagem
-        ras_meta['dtype'] = rasterio.float32
-        ras_meta['count'] = 1
-
-        if control == 0:
-            # Cálculo do índice termal de filossilicatos (Vicente & Souza Filho, 2010)
-            phyllosilicates = AST10/AST11*AST12
-            phyllosilicates = np.ma.masked_invalid(phyllosilicates)
-            with rasterio.open("Teste\\" + file + '_Phyll.tif', 'w', **ras_meta) as dst1:
-                # Define o array da banda como o array phyll
-                dst1.write(phyllosilicates.astype(rasterio.float32), 1)
-                dst1.set_band_description(1, descriptions[0]) # Escreve a descrição de cada banda
-
-        elif control == 1:
-            # Cálculo do índice termal de vegetação não fotossinteticamente ativa (Vicente et al., 2017)
-            npvthm = AST11/(AST13 + AST14)
-            npvthm = np.ma.masked_invalid(npvthm)
-            with rasterio.open("Teste\\" + file + '_Npv.tif', 'w', **ras_meta) as dst1:
-                # Define o array da banda como o array npv
-                dst1.write(npvthm.astype(rasterio.float32), 1)
-                # Escreve a descrição da banda
-                dst1.set_band_description(1, descriptions[1])
-
-        elif control == 2:
-            # Cálculo do índice termal de quartzo (Rockwell & Hofstra, 2008)
-            quartz = AST11/(AST10 + AST12)*AST13/AST12
-            quartz = np.ma.masked_invalid(quartz)
-            with rasterio.open("Teste\\" + file + '_Qtz.tif', 'w', **ras_meta) as dst1:
-                # Define o array da banda como o array qtz
-                dst1.write(quartz.astype(rasterio.float32), 1)
-                # Escreve a descrição da banda
-                dst1.set_band_description(1, descriptions[2])
-
-        ds.close()
-
-        # Shows the original image
-        img_in = f"Teste\\{file}_{type}.tif"
-        min_1, max_1 = ap.histogram.get_histogram_range(img_in)
-        subprocess.call(['gdal_translate.exe', '-ot', 'Byte', '-quiet', '-scale', str(min_1), str(max_1), img_in, f"Teste\\{file}original_{type}.tif"])
-
-        # Makes the threshold clipping
-        subprocess.call(['gdal_translate.exe', '-ot', 'Byte', '-quiet' ,'-scale', str(min), str(max), img_in, f"Teste\\{file}processed_{type}.tif"])
-
-        img = cv2.imread(f"Teste\\{file}original_{type}.tif")
+        """
         img2 = cv2.imread(f"Teste\\{file}processed_{type}.tif")
-
         # Apllies the median filter
         #img2 = cv2.medianBlur(img2, 3)
-
-        cv2.namedWindow(file + f"_Original_{type}", cv2.WINDOW_NORMAL)
-        cv2.namedWindow(file + f"_Processed_{type}", cv2.WINDOW_NORMAL)
-
-        cv2.imshow(file + f"_Original_{type}", img)
-        cv2.imshow(file + f"_Processed_{type}", img2)
-
+        cv2.namedWindow(file + "_Processed", cv2.WINDOW_NORMAL)
+        cv2.imshow(file + "_Processed", img2)
         # Tries to open the image manually processed
         try:
-            im = Image.open(f"03_Histograma\\0{control + 1}_{type}\\{file}_Histo_{type}.tif")
-            im.show()
-
         except FileExistsError:
             pass
-
-        cv2.waitKey()
-
         cv2.destroyAllWindows()
-
         folder = 'Teste\\'
         for filename in os.listdir(folder):
             file_path = os.path.join(folder, filename)
@@ -263,9 +177,63 @@ for file in files:
             except Exception as e:
                 #print('Failed to delete %s. Reason: %s' % (file_path, e))
                 pass
-
-        if control == 2:
-            control = 0
-            break
         control += 1
-    print(f"saiu do while")
+        """
+
+def compare(file: str=None, type: str=None):
+    if not type:
+        return print(f"=== Please inform a type ===")
+
+    if not file:
+        return print(f"=== Please inform a file ===")
+
+    # Check if the file is already unziped
+    if file not in os.listdir("00_Arquivos"): # Verifica se o arquivo zip já foi extraido
+        shutil.unpack_archive(f"00_Arquivos\\{file}.zip", f"00_Arquivos\\{file}")
+        print(f"file unpacked")
+
+    out_folder = f"Teste\\{file}_{type}\\"
+    try:
+        os.mkdir(f"Teste\\{file}_{type}\\")
+        os.mkdir(out_folder)
+    except FileExistsError:
+        pass
+
+    if type == 'phyll':
+        dir = '01_Phyll'
+
+    if type == 'npv':
+        dir = '02_Npv'
+
+    if type == 'qtz':
+        dir = '03_Qtz'
+
+    # 02_Indices\01_Phyll\AST_L1B_00301022014012358_20200213112936_20968_Phyll.tif
+    layer_dir = f"01_Layer_Stacking\\{file}_Layer_Stacking.tif"
+    index_dir = f"02_Indices\\{dir}\\{file}_{type.capitalize()}.tif"
+
+    # Checks if there is index image
+    if not os.path.isfile(index_dir):
+        if not os.path.isfile(layer_dir):
+            #make layer stack image
+            print(f"***********************")
+            print(f"Fazendo Layer Stack para {file}")
+            ap.layer_stack(file, '')
+        # makes index image
+        print(f"Fazendo Índice")
+        ap.index_calc(file, '')
+
+    period = get_period(file)
+    months = get_months(file)
+
+    # Makes the threshold clipping using only period
+    min, max = get_min_max(type, period)
+    subprocess.call(['gdal_translate.exe', '-ot', 'Byte', '-quiet' ,'-scale', str(min), str(max), index_dir, f"{out_folder}\\{file}_{type}_Period.tif"])
+    print(f"1 - min: {min} max: {max}")
+    # Makes the threshold clipping using period and months
+    min, max = get_min_max(type, period, months)
+    subprocess.call(['gdal_translate.exe', '-ot', 'Byte', '-quiet' ,'-scale', str(min), str(max), index_dir, f"{out_folder}\\{file}_{type}_Periodo_e_Meses.tif"])
+    print(f"2 - min: {min} max: {max}")
+
+#compare("AST_L1B_00308132009131803_20200213112625_13869", "phyll")
+auto_process()
